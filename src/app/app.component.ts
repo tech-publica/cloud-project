d9033274f29ab2ec3212060ed9466c5eefb2c54c
tech publica <tech.publica@gmail.com>
2024-02-16 03:28:06 +0100

    adds connection to spring boot backend on aws

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+
+interface Score {
+  id: number,
+  house: string,
+  currentScore: number
+}
+
 
 @Component({
   selector: 'app-root',
@@ -8,6 +17,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  URL="http://hogwarts-be-env.eba-2hfamsmz.eu-central-1.elasticbeanstalk.com/api/score";
   title = 'cloud-project';
+  scores: Score[] =[];
+  constructor(private http:HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get<Score[]>(this.URL).subscribe({
+      next: ss => this.scores = ss,
+      error: err => console.log(err)
+    });
+  }
+  
+
+
+
 }
